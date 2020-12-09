0ee5d9d6be0ac857dc408d2183d1943e76a2f550
Gabriel Demes <gabrieldemes@nyu.edu>
2020-12-09 15:34:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  value_test = 0
+  key_test = ""
+  name_hash.each do |key, value|
+    if value < value_test
+      value_test = value
+      key_test = key
+    end
+  key_test
+
 
 end
